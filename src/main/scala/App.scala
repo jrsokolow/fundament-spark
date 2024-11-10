6e35083289b11df52cca24bff724e3de22b06ea0
Jakub Sokołowski <jrsokolow@gmail.com>
2024-11-10 21:41:33 +0100

    working first app

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,3 +1,23 @@
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
 object App {
+
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().appName("fundament-spark").master("local").getOrCreate()
+
+    import spark.implicits._
+
+    val people: Seq[(String, String, Int)] = Seq(
+      ("marek", "czuma", 42),
+      ("atos", "aramis", 10),
+      ("kot", "mlot", 100),
+      ("reks", "kleks", 149)
+    )
+
+    val peopleDF:Dataset[Row] = people.toDF("firstName", "lastName", "age")
+
+    peopleDF.show()
+    peopleDF.select("firstName", "lastName").show()
+  }
 
 }
